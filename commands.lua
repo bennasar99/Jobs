b25612b80b1907119aeedf044245bfb820a0c365
tonibm19 <tonibm19@gmail.com>
2013-12-28 21:00:15 +0100

    Basic commands skeleton and added /jobs browse command.

--- commands.lua
+++ commands.lua
@@ -1,4 +1,10 @@
 function HandleJobsCommand( Split, Player )
   if (Split[2] == nil) then
-     Player:SendMessage("Usage: /jobs <browse/join/leave>
+    Player:SendMessage("Usage: /jobs <browse/join/leave>
+  elseif (Split[2] == "browse") then
+    Player:SendMessage("Jobs: Miner, Farmer, Hunter, TreeCutter")
+  elseif (Split[2] == "join") then
+    -- TODO --
+  elseif (Split[2] == "leave") then
+    -- TODO --
   end
