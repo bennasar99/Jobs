2f8f83339aee2476d5bdaeaccd2f6cfa195ce0b5
tonibm19 <tonibm19@gmail.com>
2013-12-28 17:17:52 +0100

    Initialize function

---
+++ main.lua
@@ -0,0 +1,15 @@
+function Initialize(Plugin)
+	Plugin:SetName("Jobs")
+	Plugin:SetVersion(0)
+	
+  cPluginManager.BindCommand("/jobs",                    "jobs.jobs",                  HandleJobsCommand,                        " - Join, browse or leave jobs.");
+
+	cPluginManager.AddHook(cPluginManager.HOOK_BLOCK_TO_PICKUPS, OnDigged);
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_PLACED_BLOCK, OnPlacedBlock)
+	cPluginManager.AddHook(cPluginManager.HOOK_KILLING, OnKilling)
+	
+  Coiny = cRoot:Get():GetPluginManager():GetPlugin("Coiny")
+
+	LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
+	return true
+end
