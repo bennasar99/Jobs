e9ae7e326dbccfd8f023c32d3976ba3dc05fcdbc
tonibm19 <tonibm19@gmail.com>
2013-12-31 12:21:14 +0100

    Added /jobs info
    Currently only displays job you currently have.
    When levels are implemented, it will also show your current level.

--- commands.lua
+++ commands.lua
@@ -1,8 +1,5 @@
 function HandleJobsCommand( Split, Player )
-    local UsersIni = cIniFile()
-    if UsersIni:ReadFile("users.ini") == false then
-        LOG( "Could not read users.ini!" )
-    end
+    UsersIni = cIniFile()
     if #Split < 2 then
         Player:SendMessage("Usage: /jobs <browse/join/leave>")
         return true
@@ -10,10 +7,11 @@ function HandleJobsCommand( Split, Player )
         Player:SendMessage("Jobs: miner, farmer, hunter, treecutter")
         return true
     elseif (Split[2] == "join") then
+        UsersIni:ReadFile("users.ini")
         if #Split < 3 then
             Player:SendMessage('Usage:/jobs join [jobname]')
             return true
-        elseif (Split[3] == "miner") or (Split[3] == "farmer") or (Split[3] == "hunter") or (Split[3] == "treecuter") then
+        elseif (Split[3] == "miner") or (Split[3] == "farmer") or (Split[3] == "hunter") or (Split[3] == "treecutter") then
             if (UsersIni:GetValue(Player:GetName(),   "Job") ~= "") then
                 Player:SendMessage("You already have a job!, if you want to change your job, type /jobs leave before")
                 return true
@@ -28,10 +26,21 @@ function HandleJobsCommand( Split, Player )
             return true
         end
     elseif (Split[2] == "leave") then
+        UsersIni:ReadFile("users.ini")
         if (UsersIni:GetValue(Player:GetName(),   "Job") ~= "") then
             UsersIni:DeleteValue(Player:GetName(),   "Job")
             UsersIni:WriteFile("users.ini")
             Player:SendMessage("You left your job!")
+            return true
+        else
+            Player:SendMessage("You don't have a job!, use /jobs join [jobname] and start working!")
+            return true
+        end    
+     elseif (Split[2] == "info") then
+        UsersIni:ReadFile("users.ini")
+        if (UsersIni:GetValue(Player:GetName(),   "Job") ~= "") then
+            job = UsersIni:GetValue(Player:GetName(),   "Job")
+            Player:SendMessage("You're are working as a " .. job)
             return true
         else
             Player:SendMessage("You don't have a job!, use /jobs join [jobname] and start working!")
