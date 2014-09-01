82725346883014c3026fc3cb24b3e3154294ea60
tonibm19 <tonibm19@gmail.com>
2014-09-01 12:45:38 +0200

    Reduced salaries.
    Economic crisis has come... :P

--- hooks.lua
+++ hooks.lua
@@ -9,17 +9,17 @@ function OnBlockToPickups(World, Digger, BlockX, BlockY, BlockZ, BlockType, Bloc
                 item1 = Pickups:Get(0).m_ItemType
                 amount1 = Pickups:Get(0).m_ItemCount
                 if (item1 == E_ITEM_WHEAT) then
-                    Coiny:Call("GiveMoney", PlayerDigger:GetName(), 10)
+                    Coiny:Call("GiveMoney", PlayerDigger:GetName(), 4)
                     return false   
                 elseif (item1 == E_ITEM_POTATO) then
                     if (amount1 > 1) then
-                        result = amount1 * 5
+                        result = amount1 * 2
                         Coiny:Call("GiveMoney", PlayerDigger:GetName(), result)
                         return false       
                     end     
                 elseif (item1 == E_ITEM_CARROT) then
                     if (amount1 > 1) then
-                        result = amount1 * 5
+                        result = amount1 * 2
                         Coiny:Call("GiveMoney", PlayerDigger:GetName(), result)
                         return false       
                     end     
@@ -36,30 +36,30 @@ function OnPlayerBrokenBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockTyp
         if (BlockType == E_BLOCK_COAL_ORE) then
             Coiny:Call("GiveMoney", Player:GetName(), 10)
         elseif (BlockType == E_BLOCK_DIAMOND_ORE) or (BlockType == E_BLOCK_EMERALD_ORE) then
-            Coiny:Call("GiveMoney", Player:GetName(), 250)
+            Coiny:Call("GiveMoney", Player:GetName(), 200)
         elseif (BlockType == E_BLOCK_LAPIS_ORE) then
             Coiny:Call("GiveMoney", Player:GetName(), 50)
         elseif (BlockType == E_BLOCK_NETHER_QUARTZ_ORE) then
-            Coiny:Call("GiveMoney", Player:GetName(), 20)
+            Coiny:Call("GiveMoney", Player:GetName(), 5)
         elseif (BlockType == E_BLOCK_REDSTONE_ORE) or (BlockType == E_BLOCK_REDSTONE_ORE_GLOWING) then
-            Coiny:Call("GiveMoney", Player:GetName(), 50)
+            Coiny:Call("GiveMoney", Player:GetName(), 20)
         elseif (BlockType == E_BLOCK_IRON_ORE) or (BlockType == E_BLOCK_GOLD_ORE) then
-            Coiny:Call("GiveMoney", Player:GetName(), 50) 
+            Coiny:Call("GiveMoney", Player:GetName(), 30) 
         end
     elseif (job == "treecutter") then
         if (BlockType == E_BLOCK_LOG) or (BlockType == E_BLOCK_NEW_LOG) then
-            Coiny:Call("GiveMoney", Player:GetName(), 10)   
+            Coiny:Call("GiveMoney", Player:GetName(), 2)   
         end
     elseif (job == "farmer") then
         if (BlockType == E_BLOCK_PUMPKIN) or (BlockType == E_BLOCK_MELON) then
-            Coiny:Call("GiveMoney", Player:GetName(), 20)
+            Coiny:Call("GiveMoney", Player:GetName(), 10)
         elseif (BlockType == E_BLOCK_SUGARCANE) then
             if (World:GetBlock(BlockX, BlockY + 1, BlockZ) == E_BLOCK_SUGARCANE) and (World:GetBlock(BlockX, BlockY + 2, BlockZ) == E_BLOCK_SUGARCANE) then
-                Coiny:Call("GiveMoney", Player:GetName(), 12)
+                Coiny:Call("GiveMoney", Player:GetName(), 6)
             elseif (World:GetBlock(BlockX, BlockY + 1, BlockZ) == E_BLOCK_SUGARCANE) then
-                Coiny:Call("GiveMoney", Player:GetName(), 8)
-            else
                 Coiny:Call("GiveMoney", Player:GetName(), 4)
+            else
+                Coiny:Call("GiveMoney", Player:GetName(), 2)
             end
         end   
     end 
@@ -79,13 +79,13 @@ function OnKilling(Victim, Killer)
                     elseif (Victim:IsA("cSlime")) or (Victim:IsA("cMagmaCube")) then
                         Coiny:Call("GiveMoney", Player:GetName(), 20)
                     elseif (Victim:IsA("cBlaze")) then
-                        Coiny:Call("GiveMoney", Player:GetName(), 200)          
+                        Coiny:Call("GiveMoney", Player:GetName(), 100)          
                     elseif (Victim:IsA("cEnderDragon")) then
-                        Coiny:Call("GiveMoney", Player:GetName(), 1000)       
+                        Coiny:Call("GiveMoney", Player:GetName(), 500)       
                     elseif (Victim:IsA("cWither")) then
-                        Coiny:Call("GiveMoney", Player:GetName(), 750)    
+                        Coiny:Call("GiveMoney", Player:GetName(), 200)    
                     else
-                        Coiny:Call("GiveMoney", Player:GetName(), 30)       
+                        Coiny:Call("GiveMoney", Player:GetName(), 10)       
                     end
                 end
             end
@@ -97,17 +97,17 @@ function OnPlayerPlacedBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX,
     job = UsersIni:GetValue(Player:GetName(),   "Job")
     if (job == "miner") then
         if (BlockType == E_BLOCK_IRON_ORE) or (BlockType == E_BLOCK_GOLD_ORE) then
-            Coiny:Call("TakeMoney", Player:GetName(), 50) 
+            Coiny:Call("TakeMoney", Player:GetName(), 30) 
         end
     elseif (job == "farmer") then
         if (BlockType == E_BLOCK_PUMPKIN) or (BlockType == E_BLOCK_MELON) then
-            Coiny:Call("TakeMoney", Player:GetName(), 20)
+            Coiny:Call("TakeMoney", Player:GetName(), 10)
         elseif (BlockType == E_BLOCK_SUGARCANE) then
             Coiny:Call("TakeMoney", Player:GetName(), 4)
         end   
     elseif (job == "treecutter") then
         if (BlockType == E_BLOCK_LOG) or (BlockType == E_BLOCK_NEW_LOG) then
-            Coiny:Call("TakeMoney", Player:GetName(), 20)
+            Coiny:Call("TakeMoney", Player:GetName(), 2)
         end   
     end 
 end
